f3f6210105464a70c972f2f00024fb64555e73a0
Jonathan-0207 <100753984+Jonathan-0207@users.noreply.github.com>
2022-03-18 16:05:48 +1030

    Add files via upload

---
+++ main-2-3.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int sum_if_a_palindrome(int integers[], int length);
+int sum_elements(int integers[], int length);
+
+int main(){
+    int a1[6] = {1, 2, 0, 0, 2, 1};
+
+    std::cout<< sum_elements(a1, 6);
+}
